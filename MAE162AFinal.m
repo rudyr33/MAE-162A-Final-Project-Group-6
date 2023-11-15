bcb56d57a09bd1bca3d18566514041fc8bf8d2c9
Pearl Klassen <dbf.ucla@gmail.com>
2023-11-15 13:02:30 -0800

    Update MAE162AFinal.m

--- MAE162AFinal.m
+++ MAE162AFinal.m
@@ -1,3 +1,4 @@
 %% MAE 162A Final Project
-% Names: Ahan Agarwal, Jackson Bullard, Dario Cardenas, Alexi Gill, Pearl Klassen, 
-%        Martin Nay, Grace Pelligrino, Rodolfo Ruiz, Lance Taylor, Mikaela Van de Heetkamp
+% Names: Ahan Agarwal, Jackson Bullard, Dario Cardenas, Alexi Gill, 
+%        Pearl Klassen, Martin Nay, Grace Pelligrino, Rodolfo Ruiz, 
+%        Lance Taylor, Mikaela Van de Heetkamp
