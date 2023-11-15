f6594d861d06255d056e50276c8412ba4b9fa79d
DBFatUCLA <72041438+DBFatUCLA@users.noreply.github.com>
2023-11-15 13:05:17 -0800

    Update MAE162AFinal.m

--- MAE162AFinal.m
+++ MAE162AFinal.m
@@ -1,4 +1,6 @@
-%% MAE 162A Final Project
+%% MAE 162A Final Project Code
 % Names: Ahan Agarwal, Jackson Bullard, Dario Cardenas, Alexi Gill, 
 %        Pearl Klassen, Martin Nay, Grace Pelligrino, Rodolfo Ruiz, 
 %        Lance Taylor, Mikaela Van de Heetkamp
+% Due: December 15, 2023 at 10 A.M.
+% Goal: Design a mechanism that fits in a 
