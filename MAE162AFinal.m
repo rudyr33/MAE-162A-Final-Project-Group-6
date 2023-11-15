0223729d7aec2092708f20c0e43711083290d446
DBFatUCLA <72041438+DBFatUCLA@users.noreply.github.com>
2023-11-15 13:01:50 -0800

    Update MAE162AFinal.m

--- MAE162AFinal.m
+++ MAE162AFinal.m
@@ -1,2 +1,3 @@
-%% MAE 162A Final Project %%
-%% Hello %%
+%% MAE 162A Final Project
+% Names: Ahan Agarwal, Jackson Bullard, Dario Cardenas, Alexi Gill, Pearl Klassen, 
+%        Martin Nay, Grace Pelligrino, Rodolfo Ruiz, Lance Taylor, Mikaela Van de Heetkamp
