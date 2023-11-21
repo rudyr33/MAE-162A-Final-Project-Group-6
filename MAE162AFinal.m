804f3722a84ab78b80c1ccfa0679996baedb1d78
Pearl Klassen <dbf.ucla@gmail.com>
2023-11-21 10:19:43 -0800

    Update MAE162AFinal.m

--- MAE162AFinal.m
+++ MAE162AFinal.m
@@ -2,5 +2,13 @@
 % Names: Ahan Agarwal, Jackson Bullard, Dario Cardenas, Alexi Gill, 
 %        Pearl Klassen, Martin Nay, Grace Pelligrino, Rodolfo Ruiz, 
 %        Lance Taylor, Mikaela Van de Heetkamp
+% Class: MECH&AE 162A
+% Instructor: Christopher Matthes
 % Due: December 15, 2023 at 10 A.M.
-% Goal: Design a mechanism that fits in a 
+% Goal: Design a mechanism that fits in a 850mm x 1050mm footprint in one
+%       configuration. The mechanism must convert an angular input into a
+%       (nearly) pure translational output. The translation must be at
+%       least 850mm along the x-axis, with less than 4mm of vertical
+%       translation of the table's center of mass along the y-axis. In
+%       addition, the table cannot tilt more than 1deg above or below the
+%       horizontal.
